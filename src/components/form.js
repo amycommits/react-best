f12aed7a912264f0f2b4946a60866c527fffc918
Amy Detwiler <detwiler.amy@gmail.com>
2021-04-12 22:48:31 -0400

    implemented the text area

--- src/components/form.js
+++ src/components/form.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TextInput from './FormFields/TextInput'
 import DateInput from './FormFields/DateInput'
+import TextArea from './FormFields/TextArea'
 export default function App() {
   return (
     <div className="App">
@@ -8,6 +9,7 @@ export default function App() {
       <TextInput name="Job Title" />
       <TextInput name="Job Description" />
       <DateInput name="Date Applied" />
+      <TextArea name="First Impression" />
     </div>
   );
 }
